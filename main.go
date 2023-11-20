626f745d2e8af7320f70197e97fefeb7631c75e5
Denis GERMAIN <dt.germain@gmail.com>
2023-11-20 16:59:30 +0100

    chore: add accordion

--- main.go
+++ main.go
@@ -10,7 +10,23 @@ func main() {
 	proxfyne := app.New()
 	mainWindow := proxfyne.NewWindow("proxfyne")
 
-	mainWindow.SetContent(widget.NewLabel("Hello World!"))
+	displayMenu(mainWindow)
+
 	mainWindow.Resize(fyne.NewSize(800, 600))
 	mainWindow.ShowAndRun()
+}
+
+func displayMenu(window fyne.Window) {
+	ac := createAccordion()
+
+	window.SetContent(ac)
+}
+
+func createAccordion() fyne.Widget {
+	ac := widget.NewAccordion(
+		widget.NewAccordionItem("server1", widget.NewLabel("vm1")),
+		widget.NewAccordionItem("server2", widget.NewLabel("vm2")),
+	)
+	ac.MultiOpen = true
+	return ac
 }
